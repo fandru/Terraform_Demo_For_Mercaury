01b9b49e7fa4e394a10024a6348f1e357ea3470a
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-04 17:57:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
-  location      = "EU"
+  location = "EU"
 
 }
 
 
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
-  location      = "EU"
+  location = "EU"
 
 }
 
